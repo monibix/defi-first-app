2ae7d797f07b3ce0e7eeb2226de5bec63f60492f
monibix <monibixv2@gmail.com>
2021-11-01 23:16:45 +0100

    token created&continued tether contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 const Tether = artifacts.require('../src/contracts/Tether.sol');
+const Faml = artifacts.require('../src/contracts/Faml.sol')
 
 module.exports = async function(deployer) {
     await deployer.deploy(Tether)
+    await deployer.deploy(Faml)
 };
+
--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -5,4 +5,37 @@ contract Tether {
     string public symbol = 'mUSDT';
     uint256 public totalSupply = 1000000000000000000000000; //18 zeros + 6 zeros pel milió. 1 milion tokens
     uint8 public decimals = 18;
+
+    event Transfer (
+        address indexed _from,
+        address indexed _to,
+        uint _value
+    );
+
+    event Approve (
+        address indexed _owner, 
+        address indexed _spender, 
+        uint _value
+    );
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint _value) public returns(bool success) {
+        //check if the balance is greater or equal than the value to transfer
+        require(balanceOf[msg.sender] >= _value);
+        //substract the value to the sender
+        balanceOf[msg.sender] -= _value;
+        //add the value to transferee 
+        balanceOf[_to] += _value;
+        //run the transfer
+        emit Transfer(msg.sender, _to, _value);
+        //return true if everything is successful 
+        return true;
+    }
+    
+
 }
