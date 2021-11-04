6cda7e832f0b3b402ee70056f9fc43d25cccafab
monibix <monibixv2@gmail.com>
2021-11-04 21:15:05 +0100

    finished Tether token

--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 contract Tether {
     string public name = 'Mock Tether Token';
     string public symbol = 'mUSDT';
-    uint256 public totalSupply = 1000000000000000000000000; //18 zeros + 6 zeros pel milió. 1 milion tokens
+    uint256 public totalSupply = 1000000000000000000000000;
     uint8 public decimals = 18;
 
     event Transfer (
@@ -12,19 +12,20 @@ contract Tether {
         uint _value
     );
 
-    event Approve (
+    event Approval (
         address indexed _owner, 
         address indexed _spender, 
         uint _value
     );
 
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor() {
         balanceOf[msg.sender] = totalSupply;
     }
 
-    function transfer(address _to, uint _value) public returns(bool success) {
+    function transfer(address _to, uint256 _value) public returns(bool success) {
         //check if the balance is greater or equal than the value to transfer
         require(balanceOf[msg.sender] >= _value);
         //substract the value to the sender
@@ -36,6 +37,21 @@ contract Tether {
         //return true if everything is successful 
         return true;
     }
+
+    function approve(address _spender, uint256 _value) public returns(bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
     
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]); 
+        require(_value <= allowance[_from][msg.sender]);
+        balanceOf[_to] += _value; 
+        balanceOf[_from] -= _value;
+        allowance[msg.sender][_from] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
 
 }
