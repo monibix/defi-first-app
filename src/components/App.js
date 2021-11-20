ac421b1f09d012787f769f26321dee103963bfec
monibix <monibixv2@gmail.com>
2021-11-20 14:09:23 +0100

    updating state

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import Navbar from './Navbar';
 import Web3 from 'web3';
+import Tether from '../truffle_abis/Tether.json';
+import RWD from '../truffle_abis/RWD.json';
+import DecentralBank from '../truffle_abis/DecentralBank.json';
 
 class App extends Component {
     
@@ -32,7 +35,39 @@ class App extends Component {
         const web3 = window.web3
         const account = await web3.eth.requestAccounts()
         this.setState({account: account[0]})
-        console.log(account)
+        const networkId = await web3.eth.net.getId()
+        //Load Tether Contract
+        let tetherData = Tether.networks[networkId]
+        console.log("tetherData", tetherData)
+        if (tetherData) {
+            const tether = new web3.eth.Contract(Tether.abi, tetherData.address)
+            this.setState({tether: tether})
+            let tetherBalance = await tether.methods.balanceOf(this.state.account).call()
+            this.setState({tetherBalance: tetherBalance.toString()})
+        } else  {
+            window.alert('Error! Tether contract not deployed - no detected network!')
+        }
+        //Load RWD Contract
+        let rwdData = RWD.networks[networkId]
+        if (rwdData) {
+            const rwd = new web3.eth.Contract(RWD.abi, rwdData.address)
+            this.setState({rwd: rwd})
+            let rwdBalance = await rwd.methods.balanceOf(this.state.account).call()
+            this.setState({rwdBalance: rwdBalance.toString()})
+        } else  {
+            window.alert('Error! RWD contract not deployed - no detected network!')
+        }
+        //Load DecentralBank Contract
+        let decentralBankData = DecentralBank.networks[networkId]
+        if (decentralBankData) {
+            const decentralBank = new web3.eth.Contract(DecentralBank.abi, decentralBankData.address)
+            this.setState({decentralBank: decentralBank})
+            let stakingBalance = await decentralBank.methods.stakingBalance(this.state.account).call()
+            this.setState({stakingBalance: stakingBalance.toString()})
+        } else  {
+            window.alert('Error! DecentralBank contract not deployed - no detected network!')
+        }
+        this.setState({loading: false})
     }
 
     constructor(props) {
@@ -49,11 +84,15 @@ class App extends Component {
             loading: true //for a better UX 
         }
     }
+    
 
     render() {
         return (
             <div>
                 <Navbar account={this.state.account}></Navbar>
+                <div>
+                    <h1>{console.log(this.state.loading)}</h1>
+                </div>
             </div>
         )
     }
